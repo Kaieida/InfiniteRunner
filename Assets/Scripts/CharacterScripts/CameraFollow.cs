9b715b62ff5f4555c1b3b7f0a6bb11c73fa4752d
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-11-06 21:28:01 +0200

    Initial commit

---
+++ Assets/Scripts/CharacterScripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform = null;
+    void Update()
+    {
+        transform.position = _playerTransform.position + new Vector3(0, 25, 50);
+    }
+}
---
+++ Assets/Scripts/CharacterScripts/CharactersController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersController : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 10;
+    [SerializeField] private float _jumpForce = 10;
+    [SerializeField] private Animator _animator = null;
+    [SerializeField] private Rigidbody _rigidBody = null;
+    private bool _isGrounded;
+    private bool _wasGrounded;
+    private bool _jumpInput;
+    void Update()
+    {
+        if (!_jumpInput && Input.GetKey(KeyCode.Space))
+        {
+            _jumpInput = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        _animator.SetBool("Grounded", _isGrounded);
+        transform.position += transform.forward * _movementSpeed * Time.deltaTime;
+        _animator.SetFloat("MoveSpeed", _movementSpeed);
+        JumpingAndLanding();
+        _wasGrounded = _isGrounded;
+        _jumpInput = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            _isGrounded = false;
+        }
+    }
+    private void JumpingAndLanding()
+    {
+        //bool jumpCooldownOver = (Time.time - jumpTimeStamp) >= minJumpInterval;
+
+        /*if (jumpCooldownOver && _isGrounded && _jumpInput)
+        {
+             jumpTimeStamp = Time.time;
+            _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }*/
+        if (_isGrounded && _jumpInput)
+        {
+            _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+        if (!_wasGrounded && _isGrounded)
+        {
+            _animator.SetTrigger("Land");
+        }
+        if (!_isGrounded && _wasGrounded)
+        {
+            _animator.SetTrigger("Jump");
+        }
+    }
+}
