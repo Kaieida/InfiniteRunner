32667b621f0ba1e800598e9285416bcccc8558ac
Kaieida <63949971+Kaieida@users.noreply.github.com>
2020-11-09 19:19:55 +0200

    Chunk generation system.

--- Assets/Scripts/CharacterScripts/CameraFollow.cs
+++ Assets/Scripts/CharacterScripts/CameraFollow.cs
@@ -7,6 +7,6 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform _playerTransform = null;
     void Update()
     {
-        transform.position = _playerTransform.position + new Vector3(0, 25, 50);
+        transform.position = _playerTransform.position + new Vector3(0, 25, -50);
     }
 }
---
+++ Assets/Scripts/GameScripts/TileManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private List<Tiles> spawnedTiles;
+    [SerializeField] private List<Tiles> tilePrefabs;
+    [SerializeField] private Transform _player;
+    public Tiles FirstTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedTiles.Add(FirstTile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_player.position.z + " and " + spawnedTiles[spawnedTiles.Count - 1].End.position.z);
+        if(_player.position.z > spawnedTiles[spawnedTiles.Count - 1].End.position.z)
+        {
+            SpawnTile();
+        }
+    }
+    private void SpawnTile()
+    {
+        Tiles newTiles = Instantiate(tilePrefabs[Random.Range(0,tilePrefabs.Count)]);
+        newTiles.transform.position = spawnedTiles[spawnedTiles.Count-1].End.position- newTiles.Begin.localPosition;
+        spawnedTiles.Add(newTiles);
+    }
+}
